07a29fb61433ea56f0450886836981792b32e595
이기태 <91439717+kitaee@users.noreply.github.com>
2023-02-27 08:39:56 +0900

    Update 1697.py

--- 1697.py
+++ 1697.py
@@ -1,29 +1,21 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
 from collections import deque
 
-visited = [False for _ in range(100001)]
-
 N,K = map(int, input().split())
-maxLength = max(N,K)
+MAX = 200000
+visited = [False for _ in range(MAX)]
+visited[N] = True
 queue = deque()
 queue.append((N,0))
-visited[N] = True
 
 while queue:
-  current, count = queue.popleft()
+  current, time = queue.popleft()
   if current == K:
-    print(count)
+    print(time)
     break
-  else:
-    if current+1 <= 100000 and visited[current+1] == False:
-      queue.append((current+1, count+1))
-      visited[current+1] = True
-    if 0 <= current-1 and visited[current-1] == False:
-      visited[current-1] = True
-      queue.append((current-1, count+1))
-    if current*2 <= 100000 and visited[current*2] == False:
-      visited[current*2] = True
-      queue.append((current*2, count+1))
+  for newPosition in [current-1, current+1, current*2]:
+    if 0 <= newPosition < MAX and visited[newPosition] == False:
+      queue.append((newPosition, time+1))
+      visited[newPosition] = True
