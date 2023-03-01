1d734993acbd9f0ce30d80084316652bf7ea9d60
이기태 <91439717+kitaee@users.noreply.github.com>
2023-03-01 21:01:22 +0900

    Update 1149.py

--- 1149.py
+++ 1149.py
@@ -13,3 +13,18 @@ for i in range(1,N):
   cost[i][2] += min(cost[i-1][0], cost[i-1][1])
 
 print(min(cost[-1]))
+
+&& 
+
+N = int(input())
+cost = []
+
+for _ in range(N):
+  cost.append(list(map(int, input().split())))
+
+for i in range(1,N):
+  cost[i][0] += min(cost[i-1][1], cost[i-1][2])
+  cost[i][1] += min(cost[i-1][0], cost[i-1][2])
+  cost[i][2] += min(cost[i-1][0], cost[i-1][1])
+
+print(min(cost[-1]))
