dc3bd3dcd4906b70217c7a8b2cc12eb3ebe9116a
이기태 <91439717+kitaee@users.noreply.github.com>
2023-03-07 06:39:47 +0900

    Update 7562.py

--- 7562.py
+++ 7562.py
@@ -6,26 +6,26 @@ from collections import deque
 dy = [-2,-2,-1,-1,1,1,2,2]
 dx = [1,-1,2,-2,2,-2,1,-1]
 
-def bfs(visited,y,x,targetY,targetX):
+def bfs(visited, current_x, current_y, target_x, target_y, I):
   queue = deque()
-  queue.append((y,x,0))
-  visited[y][x] = True
+  queue.append((current_y,current_x,0))
+  visited[current_y][current_x] = True
 
   while queue:
     yIndex, xIndex, count = queue.popleft()
-    if yIndex == targetY and xIndex == targetX:
+    if yIndex == target_y and xIndex == target_x:
       return count
     for i in range(8):
       ny = yIndex + dy[i]
       nx = xIndex + dx[i]
-      if 0<=ny<N and 0<=nx<N and visited[ny][nx] == False:
-        visited[ny][nx] = True
+      if 0<=nx<I and 0<=ny<I and visited[ny][nx] == False:
         queue.append((ny,nx,count+1))
+        visited[ny][nx] = True
 
 T = int(input())
 for _ in range(T):
-  N = int(input())
-  visited = [[False for _ in range(N)] for _ in range(N)]
-  x, y = map(int, input().split())
-  targetX, targetY = map(int, input().split())
-  print(bfs(visited,y,x,targetY,targetX))
+  I = int(input())
+  current_x, current_y = map(int, input().split())
+  target_x, target_y = map(int, input().split())
+  visited = [[False for _ in range(I)] for _ in range(I)]
+  print(bfs(visited,current_x,current_y,target_x,target_y,I))
