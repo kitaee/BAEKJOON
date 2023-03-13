016fc2e312fb54938a1f442d1b3de244a70de4cc
이기태 <91439717+kitaee@users.noreply.github.com>
2023-03-13 12:38:21 +0900

    Update 16953.py

--- 16953.py
+++ 16953.py
@@ -1,20 +1,20 @@
 import sys
-sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
 
 from collections import deque
 
 def convertNum(A,B):
   queue = deque()
   queue.append((A,1))
-
+  
   while queue:
     num, count = queue.popleft()
     if num == B:
       return count
-    if num > B:
+    elif num > B:
       continue
-    queue.append((num*2,count+1))
-    queue.append((int(str(num)+'1'),count+1))
+    queue.append((num*2, count+1))
+    queue.append((int(str(num)+'1'), count+1))
   return -1
 
 A,B = map(int, input().split())
