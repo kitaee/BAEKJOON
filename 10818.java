42545cebe0028606317209a934d2fabe012d03b5
이기태 <91439717+kitaee@users.noreply.github.com>
2023-05-15 07:25:01 +0900

    Create 10818.java

---
+++ 10818.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int N = sc.nextInt();
+      int max = -1000000;
+      int min = 1000000;
+      
+      for(int i=0; i<N; i++) {
+          int num = sc.nextInt();
+          if (num > max) {
+              max = num;
+          }
+          if (num < min) {
+              min = num;
+          }
+      }
+      
+      System.out.print(min + " " + max);
+    }
+}
