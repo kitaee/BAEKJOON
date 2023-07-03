5800fbe26a4afb842979c539c5bc5b2723a69c6b
이기태 <91439717+kitaee@users.noreply.github.com>
2023-07-04 08:48:51 +0900

    Create 15655.java

---
+++ 15655.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int N,M;
+    static int[] arr,visited,output;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] info = br.readLine().split(" ");
+        N = Integer.parseInt(info[0]);
+        M = Integer.parseInt(info[1]);
+        
+        arr = new int[N];
+        visited = new int[N];
+        output = new int[M];
+        
+        String[] inputArr = br.readLine().split(" ");
+        for(int i=0; i<inputArr.length; i++) {
+            arr[i] = Integer.parseInt(inputArr[i]);
+        }
+        
+        Arrays.sort(arr);
+        
+        dfs(0,0);
+        
+        System.out.println(sb);   
+    }
+    
+    static void dfs(int depth, int start) {
+        if(depth == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(output[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i=start; i<N; i++) {
+            if(visited[i] == 0) {
+                visited[i] = 1;
+                output[depth] = arr[i];
+                dfs(depth+1, i);
+                visited[i] = 0;
+            }
+        }
+    }
+}
